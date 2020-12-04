283dc98bddd7d7e69b937a30e5b90b23e99afe07
overfloat <rillrate@gmail.com>
2020-12-04 17:34:38 +0300

    Install rill crate

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,23 @@
-fn main() {
+use anyhow::Error;
+use futures::{select, FutureExt, StreamExt};
+use tokio::io::{self, AsyncBufReadExt, BufReader};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    rill::install("teleport")?;
+    let stdin = BufReader::new(io::stdin());
+    let mut lines = stdin.lines().fuse();
+    loop {
+        select! {
+            line = lines.next() => {
+                if let Some(line) = line {
+                    // TODO: Send line by rill
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    rill::terminate()?;
+    Ok(())
 }
