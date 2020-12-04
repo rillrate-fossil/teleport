9e86daac8109983de2062aabfc4af247906df7bb
overfloat <rillrate@gmail.com>
2020-12-04 23:22:38 +0300

    Generating corresponding providers

--- src/logparser.rs
+++ src/logparser.rs
@@ -36,26 +36,28 @@ impl LogParser {
 
     pub fn parse(&self, line: &str) -> Result<LogRecord, Error> {
         let cap = self.re.captures(&line).ok_or(LogParserError::NoMatches)?;
+
         let ts = cap.name("ts").ok_or(LogParserError::NoTimestamp)?;
         let timestamp = ts.as_str().to_owned();
-        let lvl = cap.name("lvl").ok_or(LogParserError::NoLevel)?;
-        let level = lvl.as_str().to_owned();
-        let path = cap.name("path").ok_or(LogParserError::NoPath)?;
-        let module = path.as_str().to_owned();
         let msg = cap.name("msg").ok_or(LogParserError::NoMessage)?;
         let message = msg.as_str().to_owned();
         let data = RillData::LogRecord { timestamp, message };
+
+        let lvl = cap.name("lvl").ok_or(LogParserError::NoLevel)?;
+        let path = cap.name("path").ok_or(LogParserError::NoPath)?;
         let mut path: Vec<_> = path
             .as_str()
             .split(&self.separator)
             .map(EntryId::from)
             .collect();
-        let level = EntryId::from(level);
+        let level = EntryId::from(lvl.as_str());
         path.push(level);
+
         let record = LogRecord {
             path: Path::from(path),
             data,
         };
+
         Ok(record)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod logparser;
 
 use anyhow::Error;
-use futures::{select, StreamExt};
-use logparser::LogParser;
-use rill::{pathfinder::Pathfinder, Provider};
+use futures::{select, FutureExt, StreamExt};
+use logparser::{LogParser, LogRecord};
+use rill::{
+    pathfinder::{Pathfinder, Record},
+    Provider,
+};
 use tokio::io::{self, AsyncBufReadExt, BufReader};
+use tokio::signal;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -21,16 +25,25 @@ async fn routine() -> Result<(), Error> {
     let log_parser = LogParser::build(PATTERN, "::")?;
     let stdin = BufReader::new(io::stdin());
     let mut lines = stdin.lines().fuse();
-    //let providers = Pathfinder::new();
-    let log_provider = Provider::new("stderr".into());
+    let mut providers: Pathfinder<Provider> = Pathfinder::new();
+    let ctrl_c = signal::ctrl_c().fuse();
+    tokio::pin!(ctrl_c);
     loop {
         select! {
             line = lines.next() => {
                 if let Some(line) = line.transpose()? {
                     let res = log_parser.parse(&line);
                     match res {
-                        Ok(record) => {
-                            log_provider.send(record.data);
+                        Ok(LogRecord { path, data }) => {
+                            let provider = providers.find(&path).and_then(Record::get_link);
+                            if let Some(provider) = provider {
+                                if provider.is_active() {
+                                    provider.send(data);
+                                }
+                            } else {
+                                let provider = Provider::new(path.clone());
+                                providers.dig(path).set_link(provider);
+                            }
                         }
                         Err(err) => {
                             log::error!("Can't parse line: {}", err);
@@ -40,7 +53,9 @@ async fn routine() -> Result<(), Error> {
                     break;
                 }
             }
-            // TODO: Add CtrlC
+            _ = ctrl_c => {
+                break;
+            }
         }
     }
     Ok(())
