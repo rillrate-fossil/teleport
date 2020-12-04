09abc3b41bc1f6da8cefae2f309e5929809f4177
overfloat <rillrate@gmail.com>
2020-12-04 23:27:37 +0300

    Fix by signal termination

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,18 @@ async fn main() -> Result<(), Error> {
         log::error!("Failed: {}", err);
     }
     rill::terminate()?;
+
+    // I have to use `exit` call here, because:
+    //
+    // Source: https://docs.rs/tokio/0.3.1/tokio/io/fn.stdin.html
+    // > This handle is best used for non-interactive uses, such as when a file is piped
+    // into the application. For technical reasons, stdin is implemented by using
+    // an ordinary blocking read on a separate thread, and it is impossible to cancel
+    // that read. This can make shutdown of the runtime hang until the user presses enter.
+    //
+    // Since we are here than any incoming data is useless. We can just exit the process.
+    std::process::exit(0);
+
     Ok(())
 }
 
